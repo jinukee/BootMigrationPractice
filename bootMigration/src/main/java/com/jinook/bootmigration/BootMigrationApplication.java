c3f341a08db0ee59141271e31772b86d7cea612d
jinukee <jinukee333@gmail.com>
2026-04-22 15:17:04 +0900

    boot : add main app
    @SpringBootApplication 은 아래 3가지 핵심 annotation을 포함한다.
    
    @SpringBootConfiguration
    -> 내부적으로 @Configuration 포함, Spring IoC container에 bean을 등록하는 설정 클래스임을 명시함
    @EnableAutoConfiguration
    -> Spring Boot의 핵심인 '자동 설정'을 활성화
    @ComponentScan
    -> 해당 클래스와 동일한 혹은 하위에 위치한 package 구조를 전부 뒤져 @Component, @Service, @Repository, @Controller 등이 붙은 클래스들을 찾아 bean으로 등록

---
+++ bootMigration/src/main/java/com/jinook/bootmigration/BootMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.jinook.bootmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMigrationApplication.class, args);
+    }
+
+}
